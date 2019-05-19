e34408e01a3103b6a2e5dc2cf5eec82c0efd2608
Sait ORHAN <saitorhan@outlook.com>
2019-05-20 00:03:04 +0300

    extension methotds

---
+++ CSharpEgitim/CSharpEgitim.ExtensionMethod/ExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitim.ExtensionMethod
+{
+    public static class ExtensionMethods
+    {
+        public static bool In(this int a, int b, int c)
+        {
+            return a > b && a < c;
+        }
+
+        public static string GetFirst3Char(this string s)
+        {
+            return s.Substring(0, 3);
+        }
+    }
+}
---
+++ CSharpEgitim/CSharpEgitim.ExtensionMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitim.ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 35, sayi1 = 34, sayi2 = 76;
+
+            if (sayi.In(sayi1, sayi2))
+            {
+                Console.WriteLine("Arasında");
+            }
+            else
+            {
+                Console.WriteLine("Arasında değil");
+            }
+
+            string cumle = "abcçdef";
+
+            Console.WriteLine(cumle.GetFirst3Char());
+
+            Console.ReadLine();
+        }
+    }
+}
